41699eb84b1fcc1155f0feb696d11d77a942e13e
Marvin Zhang <tikazyq@163.com>
2022-10-15 11:21:47 +0800

    fix(code): github-actions-python code error

--- 2022-10/github-actions-python/main.py
+++ 2022-10/github-actions-python/main.py
@@ -7,9 +7,18 @@ def main():
     sel = BeautifulSoup(res.content)
     rows = sel.select('article.Box-row')
     for row in rows:
-        repo_name = '/'.join(map(lambda x: x.strip(), row.select('h1 a')[0].text.strip().split('/')))
-        description = row.select('p')[0].text.strip()
-        stars = row.select('a.Link--muted')[0].text.strip()
+        try:
+            repo_name = '/'.join(map(lambda x: x.strip(), row.select('h1 a')[0].text.strip().split('/')))
+        except IndexError:
+            repo_name = ''
+        try:
+            description = row.select('p')[0].text.strip()
+        except IndexError:
+            description = ''
+        try:
+            stars = row.select('a.Link--muted')[0].text.strip()
+        except IndexError:
+            stars = ''
         print(f'{repo_name} ({stars} stars): {description}')
 
 
